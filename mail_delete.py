c797bcd94ef0a2c1ddbc4f6fb7901f72794b556b
Shige1962 <80299389+Shige1962@users.noreply.github.com>
2021-08-25 12:25:45 +0900

    made it simple

--- mail_delete.py
+++ mail_delete.py
@@ -23,19 +23,18 @@ while while_enterd:
         
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 
-if mode == 1:
-    print("Gmail is chosen") 
+def mail_delete(mail_server):
+    print( mail_server + " is chosen") 
     imap_obj = imapclient.IMAPClient("imap.gmail.com", ssl=True, ssl_context=context)
-    print('Please type your email of Google account that ends with "@gmail.com"')
+    print('Please type your email of ' + mail_server + ' account that ends with ' +domain)
     mail_ad = input("email:")
-    print('Please type your App password for gmail')
-    mail_pass = getpass.getpass("App password:")
+    print('Please type your password for ' + mail_server)
+    mail_pass = getpass.getpass("Password:")
     try: 
         imap_obj.login(mail_ad, mail_pass)
     except Exception as e:
-        print("Error message: " + e)
-        print("If you haven't created App password on your account yet,")
-        print("please refer to https://support.google.com/accounts/answer/185833?hl=en and set App password before you run this programme")
+        # print("Error message: " + e)
+        print(exception_message)
         exit()
     print("Logged in successfully")
     # pprint.pprint(imap_obj.list_folders()) # To return folders of mail in list 
@@ -67,47 +66,16 @@ if mode == 1:
     print("Logging out from the mail server")
     exit()
 
+if mode == 1:
+    domain = '"@gmail.com"'
+    exception_message = ("If you haven't created App password on your account yet, \n please refer to https://support.google.com/accounts/answer/185833?hl=en and set App password before you run this programme")
+    mail_delete("Gmail")
+
 elif mode == 2:
-    print("Yahoo Japan Mail is chosen")
-    imap_obj = imapclient.IMAPClient("imap.mail.yahoo.co.jp", ssl=True, ssl_context=context)
-    print('Please type your email of Yahoo account that ends with "@yahoo.co.jp"')
-    mail_ad = input("email:")
-    print('Please type your password for Yahoo Japan Mail')
-    mail_pass = getpass.getpass("Password:")
-    # Insert Exception later 
-    try:
-        imap_obj.login(mail_ad, mail_pass)
-    except Exception:
-        print("Email or Password is not correct. Please try again")
-        exit()
-    print("Logged in successfully")
-    imap_obj.select_folder("Inbox", readonly=False)
-    print("Please type an email address that you want to delete from your inbox")
-    delete_ad = input("email:")
-    enterd = True
-    while enterd: 
-        UIDs = imap_obj.search(["FROM", delete_ad])
-        number_of_mail = len(UIDs)
-        print("There are " + str(number_of_mail) + " mails. Would you like to delte them all?")
-        answer = input("y/n?:")
-        if answer.lower() == "y":
-            print("Deleting all the mails. This may take for a while...")
-            for delete_id in UIDs:
-                imap_obj.delete_messages(delete_id)
-        else: 
-            enterd = False
-        imap_obj.expunge()
-        print("Mails delted successfully")
-        print("Would you like to delete other mails?")
-        answer = input("y/n?:")
-        if answer.lower() == "y":
-            print("Please type an email address that you want to delete from your inbox")
-            delete_ad = input("email:")
-        else:
-            enterd = False
-    print("Logging out from the mail server")
-    imap_obj.logout()
-    exit()
+    domain = '"@gmail.com"'
+    exception_message = ("Email or passowrod is not correct. Please try again")
+    mail_delete("Yahoo Japan Mail")
+    
 elif mode == 3:
     print("Exit")
 
